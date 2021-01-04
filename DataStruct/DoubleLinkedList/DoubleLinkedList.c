cb36f9e8d86d8edeb34764b047d21f24b9ab5783
신준호 <junho1018@gmail.com>
2021-01-04 22:04:09 +0900

    DoubleLinkedList: 소멸, 삽입, 삭제, 검색 기능을 추가.

--- DataStruct/DoubleLinkedList/DoubleLinkedList.c
+++ DataStruct/DoubleLinkedList/DoubleLinkedList.c
@@ -14,7 +14,7 @@ Node* DLL_CreateNode(ElementType NewData)
 
 void DLL_DestroyNode(Node* Node)
 {
-
+	free(Node);
 }
 
 void DLL_AppendNode(Node** Head, Node* NewNode)
@@ -41,18 +41,45 @@ void DLL_AppendNode(Node** Head, Node* NewNode)
 
 void DLL_InsertAfter(Node* Current, Node* NewNode)
 {
-
+	NewNode->NextNode = Current->NextNode;
+	NewNode->PrevNode = Current;
+	Current->NextNode = NewNode;
 }
 
 void DLL_RemoveNode(Node** Head, Node* Remove)
 {
-
+	if ((*Head) == Remove)
+	{
+		if (Remove) {
+			(*Head) = Remove->NextNode;
+			if (Remove->NextNode) {
+				Remove->NextNode->PrevNode = NULL;
+			}
+		}
+	}
+	else
+	{
+		if (Remove) {
+			if (Remove->PrevNode) {
+				Remove->PrevNode->NextNode = Remove->NextNode;
+			}
+			if (Remove->NextNode) {
+				Remove->NextNode->PrevNode = Remove->PrevNode;
+			}
+		}
+	}
 
 }
 
 Node* DLL_GetNodeAt(Node* Head, int Location)
 {
-	return NULL;
+	Node* Current = Head;
+	while (Current != NULL && Location > 0)
+	{
+		Current = Current->NextNode;
+		Location--;
+	}
+	return Current;
 }
 
 int DLL_GetNodeCount(Node* Head)
