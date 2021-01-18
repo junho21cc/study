65b9160321aa1143df538c389dc7b5bdbe075d26
신준호 <junho1018@gmail.com>
2021-01-18 22:00:28 +0900

    파일이름에 띄어쓰기를 제거

--- DataStruct/Graph/Graph_Matrix.h
+++ DataStruct/Graph/Graph_Matrix.h
@@ -3,4 +3,15 @@
 
 #define MAX_NODE (10)
 
-typedef enum { DIRECTED = 0, };
+typedef enum { DIRECTED = 0, UNDIRECTED = 1} GraphType;
+
+typedef struct tagGraph
+{
+	GraphType node;
+	bool adjMatrix[MAX_NODE][MAX_NODE];
+}Graph;
+
+void create(Graph* g, GraphType node);
+void insertEdge(Graph* g, int from, int to);
+void deletEdge(Graph* g, int from, int to);
+void print(const Graph* g);
