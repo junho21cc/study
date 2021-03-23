6271f866d798fd9a23a8e5d7f2ada97c8fc42ce8
신준호 <junho1018@gmail.com>
2021-03-23 23:06:40 +0900

    input_form.php에 label태그 넣어봄

--- PHP/member/input_form.php
+++ PHP/member/input_form.php
@@ -4,8 +4,8 @@ include('head.php');
 <form action="input.php" method="GET">
     이름: <input type="text" name="name" size="4"><br>
     전화번호: <input type="text" name="phone" size="13"><br>
-    성별: <input type="radio" name="gender" value="M">남성
-    <input type="radio" name="gender" value="W">여성<br>
+    성별: <lable><input type="radio" name="gender" value="M">남성</lable>
+    <input type="radio" name="gender" value="W"><label>여성</label><br>
     나이: <input type="text" name="age" ><br>
     <input type="submit" value ="제출">
 <?php
