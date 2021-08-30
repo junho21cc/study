95b1e1c323bbc3dd8d922bce4d2f092a13d8500f
신준호 <junho1018@gmail.com>
2021-08-30 19:26:45 +0900

    02.2: variable

---
+++ CPP/book_cpp_primer_plus/chapter_02/chapter_02/02.2/carrots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+	carrots = 25;
+
+	cout << "나는 당근을 " << carrots << "개 가지고 있다." << endl;
+	carrots = carrots - 1;
+	cout << "아삭아삭, 이제 당근은 " << carrots << "개이다." << endl;
+
+	return 0;
+}
