8bfb36c942f1d232bf36167e373f7aa9e778dabf
신준호 <junho1018@gmail.com>
2022-01-02 21:38:23 +0900

    GameMakingProject_3: NumberPuzzleGame.....키보드입력 함수로 치환 남음

--- CPP/GameMakingProject/NumberPuzzleGame/main.cpp
+++ CPP/GameMakingProject/NumberPuzzleGame/main.cpp
@@ -2,8 +2,10 @@
 #include <time.h>
 #include <conio.h>
 
-#define PUZZLE_WIDTH_SIZE 3
-#define PUZZLE_HEIGHT_SIZE 3
+#define BOARD_WIDTH 3
+#define BOARD_HEIGHT 3
+
+#define BOARD_SIZE BOARD_WIDTH * BOARD_HEIGHT
 
 #define UP 72
 #define DOWN 80
@@ -12,45 +14,101 @@
 
 using namespace std;
 
-int main()
+void init_board(int number[BOARD_SIZE])
 {
-	srand((unsigned int)time(0));
-
-	int PUZZLE_SIZE = PUZZLE_WIDTH_SIZE * PUZZLE_HEIGHT_SIZE;
-
-	int Now_Position  = PUZZLE_SIZE - 1;
-
-	int Before_Position  = Now_Position ;
-
-	int is_wrong_count = 0;
-	
-	int number[PUZZLE_WIDTH_SIZE * PUZZLE_HEIGHT_SIZE];
-
-
-	for (int i = 0; i < PUZZLE_SIZE - 1; i++)
+	for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH - 1; i++)
 	{
 		number[i] = i + 1;
 	}
 
-	number[PUZZLE_WIDTH_SIZE * PUZZLE_HEIGHT_SIZE - 1] = 9999;
-	
-	for (int i = 0; i < PUZZLE_SIZE * 4; i++)
-	{
-		int index1 = rand() % (PUZZLE_SIZE - 1);
-		int index2 = rand() % (PUZZLE_SIZE - 1);
-		int temp;
+	number[BOARD_SIZE - 1] = 9999;
+}
 
-		temp = number[index1];
-		number[index1] = number[index2];
-		number[index2] = temp;
+void move_board(int number[BOARD_SIZE], int index1, int index2)
+{
+	int temp;
+
+	temp = number[index1];
+	number[index1] = number[index2];
+	number[index2] = temp;
+}
+
+void mix_board(int number[BOARD_SIZE])
+{
+	for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH * 4; i++)
+	{
+		int index1 = rand() % (BOARD_SIZE - 1);
+		int index2 = rand() % (BOARD_SIZE - 1);
+
+		move_board(number, index1, index2);
 	}
-	
+}
+
+void print_board(int number[BOARD_SIZE])
+{
+	system("cls");
+	// 그림 그리는 거
+	for (int i = 0; i < BOARD_HEIGHT; i++)
+	{
+		for (int j = 0; j < BOARD_WIDTH; j++)
+		{
+			if (number[j + i * BOARD_HEIGHT] == 9999)
+			{
+				cout << "*\t";
+			}
+			else
+			{
+				cout << number[j + i * BOARD_HEIGHT] << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int get_key_input()
+{
+	int ip = _getch();
+	return ip;
+}
+
+int check_worong_count(int number[])
+{
+	int is_wrong_count = 0;
+	for (int i = 0; i < BOARD_HEIGHT; i++)
+	{
+		for (int j = 0; j < BOARD_WIDTH; j++)
+		{
+			if (number[j + i * BOARD_HEIGHT] != j + i * BOARD_HEIGHT + 1)
+			{
+				is_wrong_count++;
+			}
+		}
+		cout << endl;
+	}
+	return is_wrong_count;
+}
+
+int main()
+{
+	// Initialization
+	srand((unsigned int)time(0));
+	int Now_Position  = BOARD_SIZE - 1;
+	int Before_Position  = Now_Position ;
+	int is_wrong_count = 0;
+	int number[BOARD_SIZE];
+
+	init_board(number);
+	mix_board(number);
 
 	// 화살표 입력받는거
 	do
 	{
-		char c;
+		print_board(number);
 
+		//int c = get_key_input();
+		//move_board(c);
+		//is_end = check_finish();
+		char c = get_key_input();
 		if (_kbhit()) 
 		{       
 			c = _getch();    
@@ -63,66 +121,35 @@ int main()
 				switch (c) 
 				{
 				case LEFT:
-					if (Now_Position % PUZZLE_WIDTH_SIZE == 0)
+					if (Now_Position % BOARD_WIDTH == 0)
 						break;
 					Now_Position --;
 					break;
 				case RIGHT:
-					if ((Now_Position + 1) % PUZZLE_WIDTH_SIZE == 0)
+					if ((Now_Position + 1) % BOARD_WIDTH == 0)
 						break;
 					Now_Position ++;
 					break;
 				case UP:
-					if (Now_Position - PUZZLE_WIDTH_SIZE < 0)
+					if (Now_Position - BOARD_WIDTH < 0)
 						break;
-					Now_Position -= PUZZLE_WIDTH_SIZE;
+					Now_Position -= BOARD_WIDTH;
 					break;
 				case DOWN:
-					if (Now_Position + PUZZLE_WIDTH_SIZE > PUZZLE_SIZE - 1)
+					if (Now_Position + BOARD_WIDTH > BOARD_SIZE - 1)
 						break;
-					Now_Position += PUZZLE_WIDTH_SIZE;
+					Now_Position += BOARD_WIDTH;
 					break;
 				}
 
-				int temp = number[Before_Position];
-				number[Before_Position] = number[Now_Position];
-				number[Now_Position] = temp;
+				move_board(number, Before_Position, Now_Position);
 			}
 		}
 
-		system("cls");
-		// 그림 그리는 거
-		for (int i = 0; i < PUZZLE_HEIGHT_SIZE; i++)
-		{
-			for (int j = 0; j < PUZZLE_WIDTH_SIZE; j++)
-			{
-				if (number[j + i * PUZZLE_HEIGHT_SIZE] == 9999)
-				{
-					cout << "*\t";
-				}
-				else
-				{
-					cout << number[j + i * PUZZLE_HEIGHT_SIZE] << "\t";
-				}
-			}
-			cout << endl;
-		}
-
-		is_wrong_count = 0;
-		for (int i = 0; i < PUZZLE_HEIGHT_SIZE; i++)
-		{
-			for (int j = 0; j < PUZZLE_WIDTH_SIZE; j++)
-			{
-				if (number[j + i * PUZZLE_HEIGHT_SIZE] != j + i * PUZZLE_HEIGHT_SIZE + 1)
-				{
-					is_wrong_count++;
-				}
-			}
-			cout << endl;
-		}
-		
+		is_wrong_count = check_worong_count(number);		
 
 	} while (is_wrong_count - 1);
+
 	cout << "정답입니다!" << endl;
 
 	return 0;
